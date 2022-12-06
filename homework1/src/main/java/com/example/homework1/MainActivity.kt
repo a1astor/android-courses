b0d63b93c93d4255655c83ac5372b36120055651
Andrey Sergeenko <a.sergeenko@intechcore.com>
2022-12-06 16:02:27 +0300

    Add homework1.

--- homework1/src/main/java/com/example/homework1/MainActivity.kt
+++ homework1/src/main/java/com/example/homework1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.homework1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+    val waterFilter: (String) -> String = {filter -> filter}
+
+    val newVF: (String, Int, Double) ->Triple<String, Int, Double> = {first, second, third -> Triple(first,second,third)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+    }}
